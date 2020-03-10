365e7ea812900e71ae0d455dd8763fea2a118bb3
Girish <pgirisha77@gmail.com>
2020-03-11 01:23:29 +0530

    Enable Eureka service discovery

--- src/main/java/com/example/toodo/application/todo/central/config/TodoCentralConfigApplication.java
+++ src/main/java/com/example/toodo/application/todo/central/config/TodoCentralConfigApplication.java
@@ -2,10 +2,12 @@ package com.example.toodo.application.todo.central.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class TodoCentralConfigApplication {
 
     public static void main(String[] args) {

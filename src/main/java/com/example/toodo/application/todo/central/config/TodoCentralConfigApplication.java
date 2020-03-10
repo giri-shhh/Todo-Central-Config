1b3282f95962108755c2ab9b7c9b8bb36a23622b
Girish <pgirisha77@gmail.com>
2020-03-10 22:05:00 +0530

    First commit

---
+++ src/main/java/com/example/toodo/application/todo/central/config/TodoCentralConfigApplication.java
@@ -0,0 +1,15 @@
+package com.example.toodo.application.todo.central.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TodoCentralConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoCentralConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toodo/application/todo/central/config/TodoCentralConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toodo.application.todo.central.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCentralConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
